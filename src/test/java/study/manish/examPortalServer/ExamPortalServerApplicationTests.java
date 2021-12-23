6c18496b0615cbdaa124fc1c0f586d80f2100ee2
Manish Sahu <manissh7352@gmail.com>
2021-12-23 19:20:37 +0530

    project initailized

---
+++ src/test/java/study/manish/examPortalServer/ExamPortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package study.manish.examPortalServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
